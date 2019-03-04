094246aac6702dd6d63686aa643614bde2a254fb
namsoo <are34@naver.com>
2019-03-04 16:26:26 +0900

    생성날짜 최신순, 늦은순 정렬버튼구현

--- IOS_boost_Album/SecondViewController.swift
+++ IOS_boost_Album/SecondViewController.swift
@@ -13,6 +13,8 @@ class SecondViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBOutlet weak var sortButton: UIButton!
+    var buttonstatus = false
     var pictures: PHFetchResult<PHAsset>!
     
     let imageManager: PHCachingImageManager = PHCachingImageManager()
@@ -28,6 +30,31 @@ class SecondViewController: UIViewController, UICollectionViewDataSource {
         flowlayout.minimumLineSpacing = 20
         flowlayout.minimumInteritemSpacing = 20 //같은행끼리 간격
         self.collectionView.collectionViewLayout = flowlayout
+        
+    }
+    
+    @IBAction func sortbtAction(_ sender: Any) {
+        
+         buttonstatus = !buttonstatus
+        
+        if buttonstatus { //최신순클릭시
+            sortButton.setTitle("과거순", for: UIControl.State.normal)
+            let reversecreationDateFet = PHFetchOptions()
+            reversecreationDateFet.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+            //생성날짜 최근거부터 정렬(내림차순)
+            pictures = PHAsset.fetchAssets(with: reversecreationDateFet)
+            collectionView.reloadData()
+            
+        } else { //과거순일때 클릭시
+             sortButton.setTitle("최신순", for: UIControl.State.normal)
+            
+            let creationDateFet = PHFetchOptions()
+            creationDateFet.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+            //생성날짜 늦은거부터 정렬 (오름차순)
+            pictures = PHAsset.fetchAssets(with: creationDateFet)
+            collectionView.reloadData()
+        }
+       
         
     }
     
@@ -51,14 +78,6 @@ class SecondViewController: UIViewController, UICollectionViewDataSource {
         return cell
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
